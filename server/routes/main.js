375595ade22ace8b19e9a3808d823d47443ad065
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-07-28 20:04:36 -0400

    fixed bug in main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -111,7 +111,7 @@ router.get('/product/:id', (req, res, next) => {
     Product.findById({ _id: req.params.id })
       .populate('category')
       .populate('owner')
-      .populate('reviews')
+      .deepPopulate('reviews.owner')
       .exec((err, product) => {
         if (err) {
           res.json({
